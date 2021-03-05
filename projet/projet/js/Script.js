7919a8e3789ab7ed620c74f24f63084a879c1b11
FeyaF <50988204+FeyaF@users.noreply.github.com>
2021-03-05 18:33:16 +0100

    Add files via upload

--- projet/projet/js/Script.js
+++ projet/projet/js/Script.js
@@ -1,18 +1,59 @@
 // JavaScript source code
-function afficherStock(){
-	document.getElementById("stock1").innerHTML="<p ><strong>8 euros</strong><br /><strong>10</strong></p>";
-	document.getElementById("stock1").style.color="red";
-	document.getElementById("stock2").innerHTML="<p ><strong>10 euros</strong><br /><strong>30</strong></p>";
-	document.getElementById("stock2").style.color="red";
-	document.getElementById("stock3").innerHTML="<p ><strong>9,08 euros</strong><br /><strong>9</strong></p>";
-	document.getElementById("stock3").style.color="red";
-	document.getElementById("stock4").innerHTML="<p ><strong>14,33 euros</strong><br /><strong>15</strong></p>";
-	document.getElementById("stock4").style.color="red";
-	document.getElementById("stock5").innerHTML="<p ><strong>5,90 euros</strong><br /><strong>20</strong></p>";
-	document.getElementById("stock5").style.color="red";
-}   
 
+var barre = document.getElementsByTagName("aside");
+
+function zoom(image){
+    var pas=50; 
+	image.width+=pas; 
+	image.height+=pas; 
+	}
+
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+	function cacherStock(){
+    var stock= document.getElementsByClassName("stockv");
+    for(var i=0; i< stock.length; i++){
+      stock[i].textContent = ""
+    }
+	}
+	
+function afficherStock(){
+  
+   var stock= document.getElementsByClassName("stockv"); 
+ 
+          
+
+   for(var i=0; i< stock.length; i++){
+     stock[i].textContent = getRandomArbitrary(10,100);
+     stock[i].style.color="red";
+   }	
+ 
+  	}
+
+  afficher=false
+  document.getElementById('valid').addEventListener("click", function(e){
+    if(!afficher){
+      afficherStock()
+      e.target.textContent = "Cacher stock"
+      
+    } 
+    else{
+      cacherStock()
+      e.target.textContent = "stock"
+    } 
+    afficher=!afficher
+  })
+
+	
+	
+	
 document.getElementById("produits").addEventListener("click",function (e){
 	e.preventDefault();
-	afficherStock();
+/*	afficherStock();
+	cacherStock();*/
+	zoom(this); 
 });
+
